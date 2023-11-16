b71572963d23b8c2db19c403700df4808011ffe1
Karun Agarwal <113603846+singhalkarun@users.noreply.github.com>
2023-11-16 05:18:48 +0000

    Update the generate function to accept invalid and valid labeled data and utilize those for generating better responses.

--- utils/data.py
+++ utils/data.py
@@ -53,13 +53,15 @@ def parse(output, parser):
 
 
 async def generate(
-    user_content,
     system_content,
     labels,
     api_key,
     model="gpt-3.5-turbo",
     temperature=1,
     max_tokens=None,
+    num_samples=5,
+    valid_data=None,
+    invalid_data=None,
 ):
     class LabeledDataset(BaseModel):
         text: str = Field(..., description="The text of the sample generated")
@@ -79,34 +81,47 @@ async def generate(
         temperature=temperature,
     )
 
+    user_content = f"Generate {num_samples} robust samples"
+
+    valid_data = json.dumps(valid_data, separators=(',', ':')).replace('},', '},\n')
+    invalid_data = json.dumps(invalid_data, separators=(',', ':')).replace('},', '},\n')
+
+
+    template = "{format_instructions} \n {text}. \n The valid labels are {labels}. \n "
+    if valid_data : template = template + "The correctly labeled data is \n {valid_data}. \n "
+    if invalid_data: template = template + "The incorrectly labeled data is \n {invalid_data}.\n "
+
+
+
     prompt = ChatPromptTemplate.from_messages(
         [
             SystemMessage(
                 content=(
                     "You are a helpful data generation assistant. You generate labeled data for other models to learn from."
                     "You only generate data for 'valid labels' that you are given. You are given a question and a list of valid labels."
+                    "You are also given a list of correctly labeled data and incorrectly labeled data. You use these to improve the data generation."
                     "The data generated should be in the format of a list of dictionaries, where each dictionary has the keys 'text' and 'label'."
                     "You enclose the JSON values with double quotes"
                 )
             ),
-            HumanMessagePromptTemplate.from_template(
-                "{format_instructions} \n {text}. \n The valid labels are [{labels}]."
-            ),
+            HumanMessagePromptTemplate.from_template(template),
         ]
     )
 
-    chain = LLMChain(llm=llm, prompt=prompt)
+    chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
 
     output = await chain.arun(
         text=user_content,
         labels=labels,
+        invalid_data=invalid_data,
+        valid_data=valid_data,
         format_instructions=parser.get_format_instructions(),
     )
 
     # output = llm(_input.to_messages())
     try:
         parsed = parse(output, parser)
-        if len(parsed) < 20:
+        if len(parsed) < num_samples:
             logger.error("Only got %d responses", len(parsed))
     except Exception as e:
         logger.error("Exception in parsing %s", str(e))
@@ -119,7 +134,7 @@ async def get_data(system_content, api_key, task, labels, num_labels=None):
     time_stamp = str(int(time.time()))
     user_content = f"Timestamp = {time_stamp}, {USER_CONTENT}"
     try:
-        res = await generate(user_content, system_content, labels, api_key)
+        res = await generate(system_content, labels, api_key)
         return res
     except Exception as e:
         logger.error("Error %e", e)
