10dfa0ad345e23c1a0f4cf789d352a8272ecfec5
radhay-1199 <radhay.1199@gmail.com>
2024-03-14 16:33:27 +0530

    Added swagger for apis

--- autotune/settings.py
+++ autotune/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "drf_yasg",
     "rest_framework",
     "workflow"
 ]
--- workflow/urls.py
+++ workflow/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from . import views
-from .views import create_workflow_with_prompt, WorkflowDetailView, PromptViewSet, WorkflowDuplicateView, \
+from .views import create_workflow_with_prompt, WorkflowDetailView, WorkflowDuplicateView, \
     WorkflowStatusView, WorkflowUpdateView, WorkflowSearchView, TaskProgressView, GenerateTaskView
 
 # router = DefaultRouter()
@@ -10,14 +10,14 @@ from .views import create_workflow_with_prompt, WorkflowDetailView, PromptViewSe
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('workflow/', create_workflow_with_prompt, name='create_workflow'),
-    path('workflow/<uuid:workflow_id>/', WorkflowDetailView.as_view(), name='workflow-detail'),
-    path('workflows/<int:workflow_id>/prompt/', views.retrieve_prompt, name='retrieve-prompt'),
-    path('workflows/<int:workflow_id>/prompt/update/', views.update_prompt, name='update-prompt'),
-    path('workflow/<workflow_id>/update/', WorkflowUpdateView.as_view(), name='update-workflow'),
-    path('workflow/<workflow_id>/duplicate/', WorkflowDuplicateView.as_view(), name='duplicate-workflow'),
-    path('workflow/q/', WorkflowSearchView.as_view(), name='search-workflow'),
-    path('workflow/status/<workflow_id>/', WorkflowStatusView.as_view(), name='workflow-status'),
+    path('', create_workflow_with_prompt, name='create_workflow'),
+    path('<uuid:workflow_id>/', WorkflowDetailView.as_view(), name='workflow-detail'),
+    path('<int:workflow_id>/prompt/', views.retrieve_prompt, name='retrieve-prompt'),
+    path('<int:workflow_id>/prompt/update/', views.update_prompt, name='update-prompt'),
+    path('<workflow_id>/update/', WorkflowUpdateView.as_view(), name='update-workflow'),
+    path('<workflow_id>/duplicate/', WorkflowDuplicateView.as_view(), name='duplicate-workflow'),
+    path('q/', WorkflowSearchView.as_view(), name='search-workflow'),
+    path('status/<workflow_id>/', WorkflowStatusView.as_view(), name='workflow-status'),
     path('generate/<uuid:workflow_id>/', GenerateTaskView.as_view(), name='generate-task'),
     path('progress/<uuid:workflow_id>/', TaskProgressView.as_view(), name='task-progress'),
     # path('', include(router.urls)),
