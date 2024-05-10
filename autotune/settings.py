d15a2666f2a9b4cf9794ef38d830a02f93d3e3c1
sooraj1002 <ksooraj162@gmail.com>
2024-05-10 13:06:54 +0530

    fix: redis url for cache

--- autotune/settings.py
+++ autotune/settings.py
@@ -180,7 +180,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": REDIS_URL,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
