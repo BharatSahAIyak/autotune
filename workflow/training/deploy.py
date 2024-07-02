4770ace2dac9d96fe55e506ff5bb94e187188722
sooraj1002 <ksooraj162@gmail.com>
2024-07-02 16:51:01 +0530

    fix: added exception handling while running github workflow

--- workflow/training/deploy.py
+++ workflow/training/deploy.py
@@ -1,10 +1,12 @@
-from celery import shared_task
-from django.conf import settings
-from celery.utils.log import get_task_logger
-from workflow.training.utils import download_model, push_to_hub
-from github import Auth as GithubAuth, Github
 import datetime
 
+from celery import shared_task
+from celery.utils.log import get_task_logger
+from django.conf import settings
+from github import Auth as GithubAuth
+from github import Github
+
+from workflow.training.utils import download_model, push_to_hub
 
 logger = get_task_logger(__name__)
 
@@ -17,13 +19,11 @@ def deploy_model(self, request_data):
     service_names = request_data["service_names"]
     try:
         model_path = download_model(repo_id=finetuned_model_id)
-        logger.info(f"Downloaded model from Hugging Face Hub: {finetuned_model_id}")
+        logger.debug(f"Downloaded model from Hugging Face Hub: {finetuned_model_id}")
         push_to_hub(
             folder_path=model_path, repo_id=deployment_model_id, repo_type="model"
         )
-        logger.info(f"Pushed model to Hugging Face Hub: {deployment_model_id}")
-
-        logger.info("Running the deployment workflow.")
+        logger.debug(f"Pushed model to Hugging Face Hub: {deployment_model_id}")
 
         run_github_workflow(
             workflow_name=gh_workflow, inputs={"service_names": service_names}
@@ -50,47 +50,56 @@ def run_github_workflow(
     branch: str = settings.AI_TOOLS_REPO_BRANCH,
     inputs: dict = {},
 ):
-    logger.info("Dispatching the workflow: {}.".format(workflow_name))
-    g = Github(auth=GithubAuth.Token(settings.GITHUB_PAT))
-    repo = g.get_repo(repo)
-    ref = repo.get_branch(branch)
-    workflow = repo.get_workflow(workflow_name)
-    now = datetime.datetime.now()
+    try:
+        logger.info("Dispatching the workflow: {}.".format(workflow_name))
+        g = Github(auth=GithubAuth.Token(settings.GITHUB_PAT))
+        repo = g.get_repo(repo)
+        ref = repo.get_branch(branch)
+        workflow = repo.get_workflow(workflow_name)
+        now = datetime.datetime.now()
 
-    is_dispatched = workflow.create_dispatch(ref=ref, inputs=inputs)
-    if not is_dispatched:
-        raise Exception("Failed to dispatch the workflow: {}.".format(workflow_name))
+        is_dispatched = workflow.create_dispatch(ref=ref, inputs=inputs)
+        if not is_dispatched:
+            raise Exception(
+                "Failed to dispatch the workflow: {}.".format(workflow_name)
+            )
 
-    logger.info("Waiting for the workflow to start.")
-    while (
-        repo.get_workflow_runs(
-            status="in_progress",
-            created=">="
-            + (now - datetime.timedelta(seconds=5)).strftime("%Y-%m-%dT%H:%M:%S+00:00"),
-        ).totalCount
-        == 0
-    ):
-        pass
-
-    run = list(
-        repo.get_workflow_runs(
-            status="in_progress",
-            created=">="
-            + (now - datetime.timedelta(seconds=5)).strftime("%Y-%m-%dT%H:%M:%S+00:00"),
-        )
-    )[0]
-
-    logger.info(f"Waiting for the workflow {run.id} to complete.")
-    while True:
-        run = repo.get_workflow_run(run.id)
-        if (
-            run.status == "completed"
-            or run.status == "failure"
-            or run.status == "cancelled"
+        logger.info("Waiting for the workflow to start.")
+        while (
+            repo.get_workflow_runs(
+                status="in_progress",
+                created=">="
+                + (now - datetime.timedelta(seconds=5)).strftime(
+                    "%Y-%m-%dT%H:%M:%S+00:00"
+                ),
+            ).totalCount
+            == 0
         ):
-            break
+            pass
 
-    logger.info(f"{run.id} : {run.status}")
-    logger.info(run.conclusion)
+        run = list(
+            repo.get_workflow_runs(
+                status="in_progress",
+                created=">="
+                + (now - datetime.timedelta(seconds=5)).strftime(
+                    "%Y-%m-%dT%H:%M:%S+00:00"
+                ),
+            )
+        )[0]
 
-    g.close()
+        logger.info(f"Waiting for the workflow {run.id} to complete.")
+        while True:
+            run = repo.get_workflow_run(run.id)
+            if (
+                run.status == "completed"
+                or run.status == "failure"
+                or run.status == "cancelled"
+            ):
+                break
+
+        logger.info(f"{run.id} : {run.status}")
+        logger.info(run.conclusion)
+
+        g.close()
+    except Exception as e:
+        logger.error(f"Failed to run the workflow: \n{e}")
